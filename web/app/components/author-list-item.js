49b8f44f289046f40eef6df192a84db108a284d6
Kaioru <keith@kaioru.co>
2023-12-28 04:00:27 +0800

    Add Author update and delete modals and handling

---
+++ web/app/components/author-list-item.js
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class AuthorListItemComponent extends Component {
+    @tracked authorNewName = "abc";
+
+    @action
+    toggleModalAuthorEdit(author) {
+        $('#modal-edit-' + author.id).modal('show');
+    }
+
+    @action
+    toggleModalAuthorDelete(author) {
+        $('#modal-delete-' + author.id).modal('show');
+    }
+
+    @action
+    updateAuthor(author) {
+        author.name = this.authorNewName;
+        author.save();
+    }
+
+    @action
+    deleteAuthor(author) {
+        author.destroyRecord();
+    }
+}
