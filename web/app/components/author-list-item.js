4c0350e584b565b722fe83995d023a76465d0e7d
Kaioru <keith@kaioru.co>
2023-12-31 21:38:48 +0800

    Add placeholder segments for task and author

--- web/app/components/author-list-item.js
+++ web/app/components/author-list-item.js
@@ -3,43 +3,39 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class AuthorListItemComponent extends Component {
-    @tracked authorNewName = "";
-    @tracked authorError = "";
+  @tracked authorNewName = '';
+  @tracked authorError = '';
 
-    @action
-    toggleModalAuthorEdit(author) {
-        this.authorNewName = author.name
-        $('#modal-edit-author-' + author.id).modal('show');
-    }
+  @action
+  toggleModalAuthorEdit(author) {
+    this.authorNewName = author.name;
+    $('#modal-edit-author-' + author.id).modal('show');
+  }
 
-    @action
-    toggleModalAuthorDelete(author) {
-        $('#modal-delete-author-' + author.id).modal('show');
-    }
+  @action
+  toggleModalAuthorDelete(author) {
+    $('#modal-delete-author-' + author.id).modal('show');
+  }
 
-    @action
-    toggleModalAuthorError(author) {
-        $('#modal-error-author-' + author.id).modal('show');
-    }
+  @action
+  toggleModalAuthorError(author) {
+    $('#modal-error-author-' + author.id).modal('show');
+  }
 
-    @action
-    updateAuthor(author) {
-        author.name = this.authorNewName;
-        author
-            .save()
-            .catch((ex) => {
-                this.authorError = JSON.parse(ex.errors[0].detail).error.message
-                this.toggleModalAuthorError(author);
-            });
-    }
+  @action
+  updateAuthor(author) {
+    author.name = this.authorNewName;
+    author.save().catch((ex) => {
+      this.authorError = JSON.parse(ex.errors[0].detail).error.message;
+      this.toggleModalAuthorError(author);
+    });
+  }
 
-    @action
-    deleteAuthor(author) {
-        author
-            .destroyRecord()
-            .catch((ex) => {
-                this.authorError = JSON.parse(ex.errors[0].detail).error.message
-                this.toggleModalAuthorError(author);
-            });
-    }
+  @action
+  deleteAuthor(author) {
+    author.destroyRecord().catch((ex) => {
+      this.authorError = JSON.parse(ex.errors[0].detail).error.message;
+      this.toggleModalAuthorError(author);
+    });
+  }
 }
---
+++ web/app/components/task-list-item.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class TaskListItemComponent extends Component {
+    @tracked taskError = '';
+
+    @action
+    toggleModalTaskDelete(task) {
+        $('#modal-delete-task-' + task.id).modal('show');
+    }
+
+    @action
+    toggleModalTaskError(task) {
+        $('#modal-error-task-' + task.id).modal('show');
+    }
+
+    @action
+    deleteTask(task) {
+        task.destroyRecord().catch((ex) => {
+            this.taskError = JSON.parse(ex.errors[0].detail).error.message;
+            this.toggleModalTaskError(task);
+        });
+    }
+}
