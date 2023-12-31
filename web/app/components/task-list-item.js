aecdca25d7928f05620ac2237f5eada36178c9ae
Kaioru <keith@kaioru.co>
2023-12-31 23:06:42 +0800

    Add task edit action

--- web/app/components/task-list-item.js
+++ web/app/components/task-list-item.js
@@ -1,25 +1,81 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class TaskListItemComponent extends Component {
-  @tracked taskError = '';
+    @service store;
+    @tracked taskError = '';
+    @tracked taskNewTitle;
+    @tracked taskNewDescription;
+    @tracked taskNewDueDate;
+    @tracked taskNewAuthorId;
 
-  @action
-  toggleModalTaskDelete(task) {
-    $('#modal-delete-task-' + task.id).modal('show');
-  }
+    @action
+    toggleModalTaskComplete(task) {
+        $('#modal-complete-task-' + task.id).modal('show');
+    }
 
-  @action
-  toggleModalTaskError(task) {
-    $('#modal-error-task-' + task.id).modal('show');
-  }
+    @action
+    toggleModalTaskEdit(task) {
+        this.taskNewTitle = task.title;
+        this.taskNewDescription = task.description;
+        $('#modal-edit-task-' + task.id).modal('show');
+    }
 
-  @action
-  deleteTask(task) {
-    task.destroyRecord().catch((ex) => {
-      this.taskError = JSON.parse(ex.errors[0].detail).error.message;
-      this.toggleModalTaskError(task);
-    });
-  }
+    @action
+    toggleModalTaskDelete(task) {
+        $('#modal-delete-task-' + task.id).modal('show');
+    }
+
+    @action
+    toggleModalTaskError(task) {
+        $('#modal-error-task-' + task.id).modal('show');
+    }
+
+    @action
+    setTaskAuthorId(id) {
+        this.taskNewAuthorId = id;
+    }
+
+    @action
+    completeTask(task) {
+        task.status = "completed";
+        task.save().catch((ex) => {
+            this.taskError = JSON.parse(ex.errors[0].detail).error.message;
+            this.toggleModalTaskError(task);
+        });
+    }
+
+    @action
+    deleteTask(task) {
+        task.destroyRecord().catch((ex) => {
+            this.taskError = JSON.parse(ex.errors[0].detail).error.message;
+            this.toggleModalTaskError(task);
+        });
+    }
+
+    @action
+    async updateTask(task) {
+        if (this.taskNewTitle === "" || this.taskNewDescription === "") {
+            this.taskError = "Title and description cannot be empty!";
+            this.toggleModalTaskError(task);
+            return;
+        }
+
+        try {
+            task.title = this.taskNewTitle;
+            task.description = this.taskNewDescription;
+            task.dueDate = new Date(this.taskNewDueDate);
+            task.authorid = await this.store.findRecord('author', this.taskNewAuthorId);
+            task.save()
+                .catch((ex) => {
+                    this.taskError = ex;
+                    this.toggleModalTaskError(task);
+                });
+        } catch (ex2) {
+            this.taskError = ex2;
+            this.toggleModalTaskError(task);
+        }
+    }
 }
